17bc2e2a6aad16eb3ef6c27e2c503a419bfbfc6c
Subeedson V <subeedson08@gmail.com>
2026-08-05 14:29:27 +0530

    Add Day 1 challenge1.py for user input and greeting

---
+++ Python/Day01/challenge1.py
@@ -0,0 +1,9 @@
+# Challenge 1
+name = input("Enter your name: ")
+age = int(input("Enter your age: ")
+department = input("Enter your department: ")
+
+print(f"Hello {name}")
+print(f"You are {age} years old")
+print(f"Department: {department}")
+print("Future AI Engineer")
