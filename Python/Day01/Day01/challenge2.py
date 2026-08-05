612b440fe4569d79e1828a51bb8a8f685f7f56ca
Subeedson V <subeedson08@gmail.com>
2026-08-05 14:42:09 +0530

    Add Day 1 Challenge 2

---
+++ Python/Day01/Day01/challenge2.py
@@ -0,0 +1,8 @@
+# Challenge 2
+n = int(input("Enter your first number: ")
+m = int(input("Enter your second number: ")
+print(f"Addition: {n+m}")
+print(f"Subtraction: {n-m}")
+print(f"Multiplication: {n*m}")
+print(f"Division: {n/m}")
+        
