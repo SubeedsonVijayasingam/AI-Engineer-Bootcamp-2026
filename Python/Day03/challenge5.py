b0df66ffd55ce9bf44259a53845723d26521ca49
SubeedsonVijayasingam <subeedson08@gmail.com>
2026-08-10 12:41:10 +0530

    Day 3 Challenge 5 - Login Validator

---
+++ Python/Day03/challenge5.py
@@ -0,0 +1,11 @@
+#challenge5
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username != "admin":
+    print("Incorrect username")
+elif password != "python123":
+    print("Incorrect password")
+else:
+    print("Login Successful")
