cfe6568dde97d941c8821421cfc764aaaded0a6f
SubeedsonVijayasingam <subeedson08@gmail.com>
2026-08-13 09:58:58 +0530

    Complete Python Day 1 Challenge 2 and 3

---
+++ Python/Day01/challenge2.py
@@ -0,0 +1,12 @@
+#challenge2 - student details
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+department = input("Enter your department: ")
+cgpa = float(input("Enter your CGPA: "))
+
+print(f"==== STUDENT DETAILS ====")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Department: {department}")
+print(f"CGPA: {cgpa}")
---
+++ Python/Day01/challenge3.py
@@ -0,0 +1,9 @@
+#challenge3 - basic calculator
+
+n = int(input("First numbe: "))
+m = int(input("Second number: "))
+
+print(f"Addition: {n+m}")
+print(f"Subtraction: {n-m}")
+print(f"Multiplication: {n*m}")
+print(f"Division: {n/m}")
