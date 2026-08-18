83e1b9a62cfa4af1cf46d5d6c7261b15f37b34b9
SubeedsonVijayasingam <subeedson08@gmail.com>
2026-08-18 22:05:10 +0530

    Complete Python Day 9 progress

---
+++ Python/Day09 - Dictionaries & Dictionary Operations/challenge1.py
@@ -0,0 +1,10 @@
+#challenge1 - Dictionary basics
+
+student = {
+    "name": "Arun",
+    "age": 20,
+    "city": "chennai"
+}
+
+for key, value in student.items():
+    print(key, value)
---
+++ Python/Day09 - Dictionaries & Dictionary Operations/challenge2.py
@@ -0,0 +1,10 @@
+#challenge2 - Add & update dictionary
+
+student = {
+    "name": "Arun",
+    "age": 20
+}
+student["city"] = "chennai"
+student["age"] = 21
+
+print(student)
